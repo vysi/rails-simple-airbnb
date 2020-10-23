3356081ad21c944e43fcd499042f001c16945fe9
akin <ways@wayss-mbp.localdomain>
2020-10-23 12:28:15 +0200

    fix

--- db/migrate/20201023092553_create_flats.rb
+++ db/migrate/20201023092553_create_flats.rb
@@ -1,11 +1,11 @@
-class CreateFlats < ActiveRecord::Migration[6.0]
+class CreateFlats < ActiveRecord::Migration[5.1]
   def change
     create_table :flats do |t|
       t.string :name
-      t.string :address
       t.text :description
       t.integer :price_per_night
       t.integer :number_of_guests
+      t.string :address
 
       t.timestamps
     end
